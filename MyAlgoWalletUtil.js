57fb0294037e24249ee12df9bb5fdcf7aaa22883
ericsharma <sharma.e@husky.neu.edu>
2023-06-12 17:36:47 -0400

    Removed algoexplorer calls

--- MyAlgoWalletUtil.js
+++ MyAlgoWalletUtil.js
@@ -1,52 +1,45 @@
-
 const constants = require('./constants.js');
 const axios = require('axios').default;
 
-let ALGOD_SERVER = 'https://node.testnet.algoexplorerapi.io';
+let ALGOD_SERVER = 'https://testnet-api.algonode.cloud';
 
-exports.connectToWallet = async function(txn) {
-    try {  
-        console.debug("connecting...");
-        console.debug(myAlgoWallet);
-        accounts = await myAlgoWallet.connect();
+exports.connectToWallet = async function (txn) {
+  try {
+    console.debug('connecting...');
+    console.debug(myAlgoWallet);
+    accounts = await myAlgoWallet.connect();
 
-        const addresses = accounts.map(account => account.address);
-        return addresses;
-    } catch (err) {
-        console.error(err);
-    }
-
+    const addresses = accounts.map((account) => account.address);
+    return addresses;
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 exports.setAlgodServer = (algod_server) => {
-    ALGOD_SERVER = algod_server;
-}
+  ALGOD_SERVER = algod_server;
+};
 
-const getParams = async() => {
-    try {
-        const response = await axios.get(ALGOD_SERVER + '/v2/transactions/params');
-        //console.log(response);
-        return response.data;
-    } catch (error) {
-        console.error(error);
-        throw new Error("getAccountInfo failed: ", error);
-    }
-
-}
-
-
+const getParams = async () => {
+  try {
+    const response = await axios.get(ALGOD_SERVER + '/v2/transactions/params');
+    //console.log(response);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw new Error('getAccountInfo failed: ', error);
+  }
+};
 
 let transactionParams = null;
 
 (async () => {
-    transactionParams = await getParams();
-})().catch(err => {
-    console.error(err);
+  transactionParams = await getParams();
+})().catch((err) => {
+  console.error(err);
 });
 
-
-exports.setTransactionFee = function(txn) {
-    txn.fee = transactionParams["min-fee"];
-	txn.flatFee = true;
+exports.setTransactionFee = function (txn) {
+  txn.fee = transactionParams['min-fee'];
+  txn.flatFee = true;
 };
-
